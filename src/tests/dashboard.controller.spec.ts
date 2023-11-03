50f7b6e8f1b093fbdfac8cbc276515e97bd0f075
Arban Hossain <arbanhossain@gmail.com>
2023-11-04 03:38:39 +0600

    AuthModule import dependency resolved

--- src/tests/dashboard.controller.spec.ts
+++ src/tests/dashboard.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DashboardController } from '../dashboard/dashboard.controller';
 import { DashboardService } from '../dashboard/dashboard.service';
+import { AuthService } from '../auth/auth.service';
+import { AuthModule } from '../auth/auth.module';
 
 describe('DashboardController', () => {
   let dashboardControl: DashboardController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule],
       controllers: [DashboardController],
       providers: [DashboardService],
     }).compile();
