c2af2a49c8aac291c6263a0196afb63c3d69760f
Nabil <almubinnabil@gmail.com>
2023-11-04 03:49:15 +0600

    fixed terraform resource naming errors

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,9 +27,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.cloud-run-tf.location
-  project     = google_cloud_run_service.cloud-run-tf.project
-  service     = google_cloud_run_service.cloud-run-tf.name
+  location    = google_cloud_run_service.cloud-run-tf-dev.location
+  project     = google_cloud_run_service.cloud-run-tf-dev.project
+  service     = google_cloud_run_service.cloud-run-tf-dev.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
