8517fe6f8ad0489976c7d7afde23d49ae4e77af0
Nabil <almubinnabil@gmail.com>
2023-11-04 04:20:52 +0600

    changed image name

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -6,12 +6,17 @@ resource "google_cloud_run_service" "cloud-run-tf-main" {
     template {
         spec {
             containers {
-                image = "gcr.io/utilitarian-bee-244108/main_image:latest"
+                image = "gcr.io/utilitarian-bee-244108/main:latest"
                 ports {
                     container_port = 3000
                 }
             }
+            container_concurrency = 2-6
         }
+    }
+
+    timeouts {
+      create = "5m"
     }
 
     autogenerate_revision_name = true
